4259a4d36f4fb394ad75fc3a1077dc32313c6187
unknown <ducthang170301@gmail.com>
2022-08-12 17:11:52 +0700

    Thu6 ngay 12

--- Homepage.py
+++ Homepage.py
@@ -12,7 +12,7 @@ if "my_input" not in st.session_state:
     st.session_state["my_input"] = ""
 
 my_input = st.text_input("Input a text here", st.session_state["my_input"])
-submit = st.button("Submit")
+submit = st.button("Submit")  
 if submit:
     st.session_state["my_input"] = my_input
     st.write("You have entered: ", my_input)
